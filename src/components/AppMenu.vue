a54710e1266dd20f46fd840d12f15dc33ca2e3ab
augustofaggionliradossantos <a.faggion.santos@gmail.com>
2024-07-24 00:04:24 +0200

    text for menu added

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
     <div>
-      <li>Burger 1</li>
-      <li>Burger 2</li>
-      <li>Burger 3</li>
+      <div class="menu-burger">
+        <div class="menu-card" v-for="burger in menuItems" :key="burger">
+          <h1>{{ burger.title }}</h1>
+          <p>{{ burger.description }}</p>
+          <ul>
+            <li v-for="menuItem in burger.menu" :key="menuItem">
+              {{ menuItem }}
+            </li>
+          </ul>
+          <a href="">Bestelle jetzt</a>
+        </div>
+        <h2>Nur 9,99 €</h2>
+      </div>
     </div>
   </div>
   <!-- Check figma gallery section as an example of menu -->
@@ -13,6 +23,32 @@
 <script>
 export default {
   name: "AppMenu",
+  data() {
+    return {
+      // Menu
+      menuItems: [
+        {
+          title: "Beff Wunder Burger",
+          image: "burger1.jpg",
+          description: "200g Beef Patty, Tomato, Lettuce, Onion, Mayo, Mustard",
+          menu: ["Burger", "Pommes", "Getränk", "Saucen", "Apfeltasche"],
+        },
+        {
+          title: "Chicken Wunder Burger",
+          image: "burger2.jpg",
+          description: "200g Chicken Patty, Tomato, Lettuce, Onion, Mayo, Mustard",
 
+          menu: ["Burger", "Pommes", "Getränk", "Saucen", "Apfeltasche"],
+        },
+        {
+          title: "Veggie Wunder Burger",
+          image: "burger3.jpg",
+          description: "200g Veggie Patty, Tomato, Lettuce, Onion, Mayo, Mustard",
+
+          menu: ["Burger", "Pommes", "Getränk", "Saucen", "Apfeltasche"],
+        },
+      ],
+    };
+  },
 };
 </script>
